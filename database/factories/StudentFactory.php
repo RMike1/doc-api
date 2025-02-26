c883108fc9f76344d65302976377a089ca12ec2c
RMike1 <71224316+RMike1@users.noreply.github.com>
2025-02-26 16:52:39 +0200

    adding student model..

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'age' => fake()->numberBetween(15, 60),
+            'student_no'=> fake()->unique()->numberBetween(1000, 9999),
+            'level' => fake()->randomElement(['one', 'two', 'three']),
+        ];
+    }
+}

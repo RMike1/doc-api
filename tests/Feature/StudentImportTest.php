3cf80c9ef036780778af6b8c20e1de9afb5736cc
rmike1 <rudasingwamike2@outlook.com>
2025-03-05 04:47:48 -0800

    feat/ adding student export test

--- tests/Feature/StudentImportTest.php
+++ tests/Feature/StudentImportTest.php
@@ -1,14 +1,45 @@
 <?php
+
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\StudentExport;
 use App\Imports\StudentImport;
 use App\Services\ExcelService;
 use Illuminate\Http\UploadedFile;
+use App\Models\Student;
 use Illuminate\Support\Facades\Storage;
-use Tests\TestCase;
+use Maatwebsite\Excel\Jobs\QueueImport;
+use App\Http\Controllers\Student\StudentController;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-uses(TestCase::class)->in(__DIR__);
+uses(RefreshDatabase::class);
 
-it('imports student data from an Excel file', function () {
-    
+beforeEach(function () {
+    Storage::fake();
+    Excel::fake();
+    $this->excelService = app(ExcelService::class);
+});
+
+
+
+describe('student-export', function () {
+    it('queues student export successfully', function () {
+        Student::factory()->count(10)->create();
+
+        $this->excelService->export();
+
+        $file = 'students.xlsx';
+
+        Excel::assertQueued($file);
+
+        Excel::assertQueued($file, function (StudentExport $export) {
+            return true;
+        });
+    });
+
+    it('validates imported Excel file', fn() =>
+    $this->postJson(route('students.import'), [
+        'file' => null,
+    ])->assertStatus(422)
+        ->assertJsonValidationErrors(['file']));
 });
